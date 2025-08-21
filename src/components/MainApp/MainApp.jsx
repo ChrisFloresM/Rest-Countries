a27ba6dad13c7619a5b6995829239021578000c8
Christian <christian.mig.flmo@gmail.com>
2025-08-21 11:38:55 -0600

    Adding search functionality by name or by region or combined

--- src/components/MainApp/MainApp.jsx
+++ src/components/MainApp/MainApp.jsx
@@ -1,12 +1,132 @@
 import SearchAndFilter from "./SearchAndFilter.jsx";
 import CountryList from "./CountryList.jsx";
+import {useEffect, useMemo, useRef, useState} from "react";
+
+const errors = {
+	404: "Country not Found",
+}
 
 export default function MainApp() {
+	const [searchTerm, setSearchTerm] = useState("");
+	const [selectedRegion, setSelectedRegion] = useState("");
+
+	function handleSearch(e) {
+		setSearchTerm(e.target.value);
+	}
+
+	function handleSelectRegion(e) {
+		setSelectedRegion(e.target.value);
+	}
+
+	const [countriesData, error, isLoading] = useCountries(searchTerm, selectedRegion);
+
 	return (
 		<main className="mainApp">
-			<SearchAndFilter/>
-			<CountryList />
+			<SearchAndFilter searchVal={searchTerm} region={selectedRegion} onChangeSearch={handleSearch} onChangeRegion={handleSelectRegion}/>
+			{ !error && isLoading && <UserFeedback message="Searching for countries..."/>}
+			{ !isLoading && error && <UserFeedback message={error}/> }
+			{ !isLoading && !error &&
+				( countriesData?.length > 0 ?
+					<CountryList currentData={countriesData}/> :
+					<UserFeedback message="Search for a country or select a region to display countries" /> )
+			}
 		</main>
 	)
 }
 
+function UserFeedback({ message }) {
+	return <p className="text-3 mainApp__placeholder-text">{ message }</p>
+}
+
+function useCountries(searchTerm, selectedRegion) {
+  const [rawData, setRawData] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+	const previousRegion = useRef("");
+
+  useEffect(() => {
+    if (!selectedRegion && searchTerm.length < 3) {
+      setRawData([]);
+      setError(null);
+      return;
+    }
+
+		if (previousRegion.current === selectedRegion && searchTerm.length < 3) {
+			return;
+		}
+
+		const controller = new AbortController();
+		const signal = controller.signal;
+
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        let url = selectedRegion ?
+					`https://restcountries.com/v3.1/region/${selectedRegion}` :
+					`https://restcountries.com/v3.1/name/${searchTerm}`;
+
+        const res = await fetch(url, { signal });
+
+        if (!res.ok) {
+          throw new Error(`Error occurred: ${errors[res.status]}`);
+        }
+
+        const currentData = await res.json();
+
+				setRawData(processData(currentData));
+				previousRegion.current = selectedRegion;
+      } catch(e) {
+        if (e.name !== "AbortError") {
+          setRawData([]);
+          setError(e.message)
+				}
+      } finally {
+				setIsLoading(false);
+      }
+    }
+
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, [selectedRegion, searchTerm]);
+
+  const filteredData = useMemo(() => {
+		if (!selectedRegion) {
+			return rawData;
+		}
+
+    if (searchTerm.length < 3) {
+      return rawData;
+    }
+
+    return  rawData.filter(country =>
+      country.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  }, [rawData, selectedRegion, searchTerm]);
+
+	useEffect(function() {
+			if(searchTerm.length >= 3 && selectedRegion && filteredData.length === 0) {
+				setError("Country not found in selected Area");
+			} else {
+				setError(null);
+			}
+	}, [filteredData, searchTerm]);
+
+  return [filteredData, error, isLoading];
+}
+
+function processData(data) {
+		return data.map(country => {
+			return {
+				name: country.name?.common ?? "Unknown",
+				population: country.population ?? 0,
+				region: country.region ?? "N/A",
+				capital: country.capital?.[0] ?? "N/A",
+				flag: country.flags?.png ?? "https://cdn-icons-png.flaticon.com/512/13434/13434972.png",
+				alt: country.flags?.alt ?? "No description avaiable",
+			}
+		});
+}
--- src/components/MainApp/SearchAndFilter.jsx
+++ src/components/MainApp/SearchAndFilter.jsx
@@ -1,32 +1,34 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 
-export default function SearchAndFilter() {
+export default function SearchAndFilter({ searchVal, onChangeSearch, region, onChangeRegion }) {
 	return (
 		<section className="mainApp__searchAndFilter">
-			<SearchBar />
-			<Filter />
+			<SearchBar searchVal={searchVal} onChange={onChangeSearch} />
+			<Filter region={region} onChange={onChangeRegion}/>
 		</section>
 	);
 }
 
-function SearchBar() {
+function SearchBar({ searchVal, onChange }) {
 	return (
 		<div className="mainApp__search-container">
 			<FontAwesomeIcon icon={faMagnifyingGlass} className="mainApp__search-icon"/>
-			<input type="text" aria-label="Search for a country" className="text-6 mainApp__search-bar" placeholder="Search for a country..."/>
+			<input type="text" aria-label="Search for a country" className="text-6 mainApp__search-bar" placeholder="Search for a country..." value={searchVal} onChange={onChange} />
 		</div>
 	)
 }
 
-function Filter() {
+function Filter({ region, onChange }) {
 	return (
-		<select className="mainApp__filter text-6" >
-			<option>Filter by Region</option>
-			<option>América</option>
-			<option>Asia</option>
-			<option>Europe</option>
-			<option>Oceania</option>
-		</select>
+		<div className="mainApp__filter-container">
+			<select className="mainApp__filter text-6" value={region} onChange={onChange}>
+				<option value="">Filter by Region</option>
+				<option value="america">America</option>
+				<option value="asia">Asia</option>
+				<option value="europe">Europe</option>
+				<option value="oceania">Oceania</option>
+			</select>
+		</div>
 	);
 }
