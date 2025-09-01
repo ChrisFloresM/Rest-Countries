9f048fbfb1be506013e155a0523abc15bfe79b69
Christian <christian.mig.flmo@gmail.com>
2025-09-01 12:50:19 -0600

    Adding theme change feature with local storage

--- src/components/Header/ButtonTheme.jsx
+++ src/components/Header/ButtonTheme.jsx
@@ -1,16 +1,45 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import styles from './ButtonTheme.module.css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faMoon} from "@fortawesome/free-solid-svg-icons";
+import {faSun} from "@fortawesome/free-solid-svg-icons";
 
 export default function ButtonTheme() {
-	const [theme, setTheme] = useState('light');
+	const [theme, setTheme] = useState(getFromLocalStorage);
 	const oppositeTheme = theme === 'light' ? 'Dark' : 'Light';
 
+	const icons = {
+		'Dark': faMoon,
+		'Light': faSun,
+	}
+
+	function handleThemeChange() {
+		setTheme(theme => theme === 'light' ? 'dark' : 'light');
+	}
+
+	useEffect(function() {
+		document.documentElement.dataset.theme = theme;
+		setToLocalStorage("theme", theme);
+	}, [theme]);
+
 	return (
-		<button type="buttom" role="Theme change button" className={`text-6 ${styles.themeChangeButton}`}>
-			<span><FontAwesomeIcon icon={faMoon} /> {`${oppositeTheme} Mode`}</span>
+		<button type="buttom" role="Theme change button" className={`text-6 ${styles.themeChangeButton}`} onClick={handleThemeChange}>
+			<span><FontAwesomeIcon icon={icons[oppositeTheme]} /> {`${oppositeTheme} Mode`}</span>
 		</button>
 	);
 }
+
+/* Local storage functions */
+function setToLocalStorage(key, item) {
+	try {
+		localStorage.setItem(key, item);
+	} catch(e) {
+		console.log(`Error saving to local storage: ${e.message}`);
+	}
+}
+
+function getFromLocalStorage() {
+	return localStorage.getItem("theme") || "light";
+}
+
